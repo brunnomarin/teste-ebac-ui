81682f29c9e6b6ed8e812e014b8a8339e88f6a5c
Brunno Marin <brunno_br@hotmail.com>
2023-10-19 17:39:17 -0300

    massa de dados em lista

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import enderecoPage from "../support/page-obejcts/endereco.page";
+const dadosEndereco = require ('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,7 +12,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
+        enderecoPage.editarEnderecoFaturamento('José', 'Gomes', 'Microsoft', 'Brasil', 'Av. São João', '200', 'Jundiaí', 'São Paulo', '045253-00', '11955552222', 'teste1@ebac.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
 
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
 
     });
 
