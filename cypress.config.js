9f6c38186a6064c4a739cc37598628d8d5d24792
Brunno Marin <brunno_br@hotmail.com>
2023-10-20 10:45:30 -0300

    alteracoes relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'egzh44',
   e2e: {
     baseUrl:"http://lojaebac.ebaconline.art.br/" , 
     setupNodeEvents(on, config) {
